4ffd0c0b54e1ce9f9f770e9b8517c8ceba4167de
Elias Kassi <eliaskassi13@gmail.com>
2024-06-25 15:27:11 -0400

    Add list_devices function. Returns dictionary of all connected devices.

--- src/lumed_tpm/powermeter.py
+++ src/lumed_tpm/powermeter.py
@@ -17,3 +17,24 @@ class powermeter:
         self.min_wavelength = None
         self.max_wavelength = None
 
+    def list_devices(self):
+        """
+        Scan all connected devices to computer
+
+        Returns
+        -------
+        dict_connected_devices, dict
+            Dictionary of all found devices. Format: {idn1:address1, idn2:address2, ...}
+        """
+        self.dict_connected_devices = {}
+        self.rm = visa.ResourceManager()
+        for addr in self.rm.list_resources():
+            try:
+                with self.rm.open_resource(addr) as instr:
+                    idn = instr.query("*IDN?").rstrip("\n")
+                    self.dict_connected_devices[idn] = addr
+            except visa.VisaIOError:
+                pass
+        # self.dict_connected_devices = {'Thorlabs,PM100USB,1910986,1.7.0':'USB0::4883::32882::1910986::0::INSTR'}
+        return self.dict_connected_devices
+
